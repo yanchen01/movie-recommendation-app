0be29319291bfc7742958d2ece395d161dce7dd8
Jason Zhang <zhang98@bu.edu>
2021-01-12 18:57:33 -0500

    login page done, next is js logic for form submission

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 const Login = () => (
-    <div>
-        <h3>Login</h3>
+    <div className='login-container'>
+        <h2 className='login-header'>Login</h2>
+        <form className='login-form'>
+            <label>
+                <p>Email:</p>
+                <input placeholder='' type='text'/>
+            </label>
+            <label>
+                <p>Password:</p>
+                <input placeholder='' type='text'/>
+            </label>
+            <div>
+                <button className='login-submit-button'>Submit</button>
+            </div>
+        </form>
+        <button className='login-back-button'>Back</button>
     </div>
 )
 
