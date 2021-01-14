46bb7f7e2aa4385a3e25359cd14641f906d414dd
Jason Zhang <zhang98@bu.edu>
2021-01-13 21:57:34 -0500

    added authentication but small bug

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 
@@ -7,16 +7,42 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const validateUser = () =>
+    const validateUser = (e) =>
     {
+        e.preventDefault();
 
+        const token = '';
+        const request_url = 'http://movierecommendation-env.eba-cgphmqy7.us-east-2.elasticbeanstalk.com/api/';
+        const api_endpoint = '/user/login';
+        const request_body = {
+            email: email,
+            password: password
+        };
+
+        // const authorization = {
+        //     headers: {
+        //         'Authorization': `Bearer ${token}`
+        //     }
+        // }
+
+        axios.post(request_url + api_endpoint, {request_body})
+        .then(result => {
+            console.log(result);
+            console.log(result.data);
+        }, 
+        {
+            headers: 
+            {
+                'Content-Type': 'application/json'
+            }
+        })
     }
 
     return(
         <div className='login-container'>
             <h2 className='login-header'>Login</h2>
             <div>{`Currently, the email is ${email} and the password is ${password}`}</div>
-            <form className='login-form'>
+            <form className='login-form' onSubmit={validateUser}>
                 <label>
                     <p>Email:</p>
                     <input placeholder='johndoe@example.com' type='text' onChange={(e) => setEmail(e.target.value)}/>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,32 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
-const SignUp = () => (
-    <div className='login-container'>
-        <h2 className='login-header'>Sign Up</h2>
-        <form className='login-form'>
-            <label>
-                <p>Name:</p>
-                <input placeholder='John Doe' type='text'/>
-            </label>
-            <label>
-                <p>Email:</p>
-                <input placeholder='johndoe@example.com' type='text'/>
-            </label>
-            <label>
-                <p>Username:</p>
-                <input placeholder='user' type='text'/>
-            </label>
-            <label>
-                <p>Password:</p>
-                <input placeholder='****' type='text'/>
-            </label>
-            <div className='submit-bar'>
-                <Link className='login-back-button' to='/'>Back</Link>
-                <button type='submit' className='login-submit-button'>Submit</button>
-            </div>
-        </form>
-    </div>
-)
+const SignUp = () => 
+{
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signUp = () => {
+        const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTAzOTY3MjcsIm5iZiI6MTYxMDM5NjcyNywianRpIjoiYzc3NGQ3ZTgtMGJiYS00NWY4LTkxODktYThkZjBiNzcyZTZjIiwiZXhwIjoxNjEwMzk3NjI3LCJpZGVudGl0eSI6ImpqIiwiZnJlc2giOnRydWUsInR5cGUiOiJhY2Nlc3MiLCJ1c2VyX2NsYWltcyI6eyJpc19hZG1pbiI6ZmFsc2V9fQ.lwEg1Y7gN0eGusZpEpgz1XwL18aloCeDGrLi4k6YtTs';
+        const request_url = '';
+        const api_endpoint = '/user/login';
+        const request_body = {
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        };
+
+        const authorization = {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }
+
+        axios.post(request_url + api_endpoint, {request_body})
+        .then(result => {
+            console.log(result);
+            console.log(result.status);
+            console.log(result.data);
+        },{
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
+    return (
+        <div className='login-container'>
+            <h2 className='login-header'>Sign Up</h2>
+            <div>{`The name is ${name}, the email is ${email}, the username is ${username}, and password is ${password}`}</div>
+            <form className='login-form'>
+                <label>
+                    <p>Name:</p>
+                    <input onChange={(e) => setName(e.target.value)} placeholder='John Doe' type='text'/>
+                </label>
+                <label>
+                    <p>Email:</p>
+                    <input onChange={(e) => setEmail(e.target.value)} placeholder='johndoe@example.com' type='text'/>
+                </label>
+                <label>
+                    <p>Username:</p>
+                    <input onChange={(e) => setUsername(e.target.value)} placeholder='user' type='text'/>
+                </label>
+                <label>
+                    <p>Password:</p>
+                    <input onChange={(e) => setPassword(e.target.value)} placeholder='****' type='text'/>
+                </label>
+                <div className='submit-bar'>
+                    <Link className='login-back-button' to='/'>Back</Link>
+                    <button type='submit' className='login-submit-button'>Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
 
 export {SignUp as default};
